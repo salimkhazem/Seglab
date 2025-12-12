744ac14eb6f0781ae138683a609dc378c2c581fd
Salim Khazem <salim.khazem@centralesupelec.fr>
2025-12-12 17:16:26 +0100

    Adding UNet

---
+++ seglab/models/unet_smp.py
@@ -0,0 +1,23 @@
+"""U-Net baseline using segmentation_models_pytorch."""
+
+from __future__ import annotations
+
+from typing import Any
+
+import segmentation_models_pytorch as smp
+
+from seglab.models import LitBinarySeg
+from seglab.utils.registry import register_model
+
+
+@register_model("unet")
+def build_unet(cfg: Any) -> LitBinarySeg:
+    net = smp.Unet(
+        encoder_name=cfg.model.encoder,
+        encoder_weights="imagenet" if cfg.model.pretrained else None,
+        in_channels=3,
+        classes=1,
+        activation=None,
+    )
+    return LitBinarySeg(net, cfg)
+
